096dcac095bc86fe173db1d3221eeac087bdf1aa
marlon nino <mvnino.halcyondigital@gmail.com>
2024-04-01 22:44:34 +0800

    build cms side

---
+++ app/auth/blogs/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthBlogs = () => {
+  return (
+    <div>AuthBlogs</div>
+  )
+}
+
+export default AuthBlogs
---
+++ components/FooterCms.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FooterCms = () => {
+  return (
+    <footer style={{ display: 'fixed', bottom: 0, }}>
+      <nav>
+        <div className="nav-links-container">
+        </div>
+      </nav>
+      <p id="copyright">Copyright © 2024 Marlon Niño. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default FooterCms;
